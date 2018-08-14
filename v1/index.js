aa198e462465956c994a32b66505bcb4b9ed969e
Akaki Mikaia <31027725+akomiqaia@users.noreply.github.com>
2018-08-14 13:52:18 +0100

    compleet redisgning

---
+++ v1/index.js
@@ -0,0 +1,8 @@
+// // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+// function openTab(tab) {
+//   let x = document.getElementsByClassName("containerTab");
+//   for (let i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//   }
+//   document.getElementById(tab).style.display = "block";
+// }
