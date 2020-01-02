4d2d8044d3cf0c9d2a05d2c5cb7481e2c993b9e9
Akaki Mikaia <akaki.mikaia.1@iliauni.edu.ge>
2020-01-02 19:59:16 +0100

    finished the carousel

--- index.js
+++ index.js
@@ -1,87 +1,77 @@
-// let slideIndex = 0
-// let slideShowTimer
-// showSlides(slideIndex)
+let slideIndex = 0  // tracking the index of the current slide
+let slideShowTimer //timer for the slideshow
+let stop = document.querySelector('.stop') // stop button
+let play = document.querySelector('.play') // start button
 
-// function plusSlides (n) {
-//   clearTimeout(slideShowTimer)
-//   showSlides((slideIndex += n))
-//   slideIndex--
-//   timedSlideShow()
-// }
 
-// // arrow navigations for the slideshow
-// document.onkeydown = function (e) {
-//   switch (e.keyCode) {
-//     case 37:
-//       plusSlides(-1)
-//       break
-//     case 39:
-//       plusSlides(1)
-//       break
-//   }
-// }
+//  function to setup the interval for looping carousel
+function timedSlideShow () {
+  slideIndex++
+  showSlides(slideIndex)
+  slideShowTimer = setTimeout(timedSlideShow, 2000)
+}
+timedSlideShow()
 
-// // function to show the slideshow
-// function showSlides (n) {
-//   const slides = document.querySelectorAll('.carousel__slide')
-//   slides.forEach((x) => {
-//     x.style.display = 'none'
-//   })
-//   if (slideIndex > slides.length || n > slides.length) {
-//     slideIndex = 1
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length
-//   }
-//   slides[slideIndex - 1].style.display = 'block'
-// }
-// //  function to setup the interval for looping carousel
-// function timedSlideShow () {
-//   slideIndex++
-//   showSlides(slideIndex)
-//   slideShowTimer = setTimeout(timedSlideShow, 7000)
-// }
-// timedSlideShow()
 
-// // play and stop button funcitonality for carousel
-
-// let carouselOn = 0
-// let stop = document.querySelector('.stop')
-// let play = document.querySelector('.play')
-
-// function startCarousel () {
-//   if (!carouselOn) {
-//     carouselOn = 1
-//     timedSlideShow()
-//     play.style.display = 'none'
-//     stop.style.display = 'block'
-//   }
-// }
-
-// function stopCarousel () {
-//   clearTimeout(slideShowTimer)
-//   carouselOn = 0
-//   stop.style.display = 'none'
-//   play.style.display = 'block'
-// }
-
-// get all the slides and the width of it.
-const track = document.querySelector('.carousel__track')
-const slides = document.querySelectorAll('.carousel__slide')
-const slideWidth = slides[0].getBoundingClientRect().width
-// arange slides next to each other
-slides.forEach((slide, index) => {
-  slide.style.left = slideWidth * index + 'px'
-})
-
-function plusSlides() {
-  let curentSlide = document.querySelector('.active__slide')
-  let nextSlide = curentSlide.nextElementSibling
-  let amountToMove = nextSlide.style.left
-  track.style.transform = 'translateX(-' + amountToMove + ')'
-
+// function that takes an integer that is either 1 or -1
+// depending if user preses next button or previous
+function plusSlides (n) {
+  clearTimeout(slideShowTimer)
+  showSlides((slideIndex += n))
+  slideIndex--
+  timedSlideShow()
 }
 
-function minusSlides() {
-
+// arrow navigations for the slideshow
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 37:
+      plusSlides(-1)
+      break
+    case 39:
+      plusSlides(1)
+      break
+  }
 }
+
+// function to show the slideshow
+function showSlides (n) {
+  const slides = document.querySelectorAll('.carousel__slide')
+  console.log(slides)
+  slides.forEach((x) => {
+    x.style.display = 'none'
+  })
+  if (slideIndex > slides.length || n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  console.log(slideIndex)
+  slides[slideIndex - 1].style.display = 'block'
+  play.style.display = 'none'
+  stop.style.display = 'block'
+}
+
+
+// play and stop button funcitonality for carousel
+
+let carouselOn = 0  // to keep the track of carousel
+
+
+function startCarousel () {
+  if (!carouselOn) {
+    carouselOn = 1
+    timedSlideShow()
+    play.style.display = 'none'
+    stop.style.display = 'block'
+  }
+}
+
+function stopCarousel () {
+  clearTimeout(slideShowTimer)
+  carouselOn = 0
+  stop.style.display = 'none'
+  play.style.display = 'block'
+}
+
