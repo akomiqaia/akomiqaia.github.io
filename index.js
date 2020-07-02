6a481862436fb6c88bbb3d7d5bb7b1251282a497
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-07-02 18:01:44 +0100

    add dropdown burger menu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // hamburger menu
-const hamburger = document.getElementById("hamburger")
-const navbar = document.querySelector(".navbar")
+const hamburger = document.getElementById('hamburger')
+const navbar = document.querySelector('.navbar')
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-active");
-  navbar.classList.toggle("navbar-is-active")
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  navbar.classList.toggle('navbar-is-active')
 })
